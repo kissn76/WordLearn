6abe497b6c230a4241a35de16494d653bcc847af
fff <kiss.norbert@contrall.hu>
2023-08-01 12:43:17 +0200

    dev

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,11 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.pagelayout import PageLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.uix.dropdown import DropDown
 from kivy.clock import Clock
 import db_sqlite as db
 
@@ -31,6 +33,93 @@ class WordlistWindow(BoxLayout):
                 elyt.ids.type_code.text = str(type_t[2])
                 elyt.ids.connection_id.text = str(type_t[3])
                 self.ids.item_list.add_widget(elyt)
+
+
+    def add(self):
+        WordAddPopup(self).open()
+
+
+class WordAddPopup(Popup):
+    def __init__(self, master, **kwargs):
+        super(WordAddPopup, self).__init__(**kwargs)
+        self.title = "Add new Word"
+        self.master = master
+
+
+    def add(self):
+        word = self.ids.word_input.text.strip()
+        type_code = self.ids.type_code_input.text.strip()
+        connection_id = self.ids.connection_id_input.text.strip()
+        # db.Database().type_add(code, name, description)
+        self.dismiss()
+        self.master.on_start()
+
+
+class WordChooser(BoxLayout):
+    def __init__(self, **kwargs):
+        super(WordChooser, self).__init__(**kwargs)
+
+        self.word = None
+
+        self.dropdown = DropDown()
+        self.mainbutton = Button(text ='Select a word...')
+        self.add_widget(self.mainbutton)
+        self.mainbutton.bind(on_release = self.dropdown.open)
+
+        self.dropdown.bind(on_select = lambda instance, x: setattr(self.mainbutton, 'text', x))
+        self.dropdown.bind(on_select = self.callback)
+
+        database = db.Database()
+        words = database.words_get()
+
+        self.word_dict = {}
+
+        if bool(words):
+            for word_t in words:
+                w = f"{str(word_t[1])} ({str(word_t[2])})"
+                self.word_dict.update({w: str(word_t[0])})
+                element = Button(size_hint_y=None)
+                element.text = w
+                element.bind(on_release=lambda element: self.dropdown.select(element.text))
+                self.dropdown.add_widget(element)
+
+
+    def callback(self, instance, x):
+        self.word = x
+        print(self.word)
+
+
+class TypeChooser(BoxLayout):
+    def __init__(self, **kwargs):
+        super(TypeChooser, self).__init__(**kwargs)
+
+        self.type = None
+
+        self.dropdown = DropDown()
+        self.mainbutton = Button(text ='Select type...')
+        self.add_widget(self.mainbutton)
+        self.mainbutton.bind(on_release = self.dropdown.open)
+
+        self.dropdown.bind(on_select = lambda instance, x: setattr(self.mainbutton, 'text', x))
+        self.dropdown.bind(on_select = self.callback)
+
+        database = db.Database()
+        types = database.types_get()
+
+        self.type_dict = {}
+
+        if bool(types):
+            for type_t in types:
+                self.type_dict.update({str(type_t[1]): str(type_t[0])})
+                element = Button(size_hint_y=None)
+                element.text = str(type_t[1])
+                element.bind(on_release=lambda element: self.dropdown.select(element.text))
+                self.dropdown.add_widget(element)
+
+
+    def callback(self, instance, x):
+        self.type = self.type_dict[x]
+        print(self.type)
 
 
 class ElemetntItem3(BoxLayout):
